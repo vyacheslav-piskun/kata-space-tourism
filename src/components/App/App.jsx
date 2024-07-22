9f8473575be1fe2227d033a60b477b4d3268e296
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-07-22 05:57:54 -0600

    add explore link styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,14 +14,20 @@ const App = () => {
 					<Typography type={FONT_TYPES.text_preset_6} center className={styles.headlineIntro}>
 						SO, YOU WANT TO TRAVEL TO
 					</Typography>
-					<Typography type={FONT_TYPES.text_preset_1} center className={styles.headlinePoint}>SPACE</Typography>
+					<Typography type={FONT_TYPES.text_preset_1} center className={styles.headlineDestination}>SPACE</Typography>
 				</p>
 				<Typography type={FONT_TYPES.text_preset_9} htmlTag="p" center className={styles.description}>
 					Let’s face it; if you want to go to space, you might as well genuinely go to
 					outer space and not hover kind of on the edge of it. Well sit back, and relax because
 					we’ll give you a truly out of this world experience!
 				</Typography>
-
+				<div className={styles.linkWrapper}>
+					<a href="#" className={styles.link}>
+						<Typography type={FONT_TYPES.text_preset_4} className={styles.linkText}>
+							explore
+						</Typography>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
