ccb6476935acb28d6baa6d47fa77c546f45e37e3
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-07-21 19:57:19 -0600

    add test header block

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as styles from './Header.module.scss';
+
+const Header = () => {
+	return (
+		<div className={styles.wrapper}>
+			<div>Header Component</div>
+		</div>
+	);
+};
+
+export default Header;
