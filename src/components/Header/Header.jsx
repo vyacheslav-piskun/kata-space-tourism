54ee03de0fb2f5c4083ce3d9fac5c257892d380b
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-07-22 20:11:20 -0600

    add logo and burger icon for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cx from 'classnames';
+
+import logoIcon from '../../assets/shared/logo.svg';
 
 import * as styles from './Header.module.scss';
 
 const Header = ({ className = '' }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<div className={cx(styles.wrapper, className)}>
-			<div>Header Component</div>
+			<img src={logoIcon} alt="logo" className={styles.logo}/>
+			<button type="button" className={cx(styles.burgerMenu, {[styles.open]: isOpen})} onClick={toggleMenu}>
+				<span />
+				<span />
+				<span />
+			</button>
 		</div>
 	);
 };
