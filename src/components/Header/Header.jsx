5d1838aa3103d03d7a73e52b71022aee690b34d4
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-07-23 06:50:41 -0600

    add animation for burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {memo, useState} from 'react';
 import cx from 'classnames';
 
 import logoIcon from '../../assets/shared/logo.svg';
@@ -6,16 +6,27 @@ import logoIcon from '../../assets/shared/logo.svg';
 import * as styles from './Header.module.scss';
 
 const Header = ({ className = '' }) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [opened, setOpened] = useState(false);
+	const [animated, setAnimated] = useState(false);
 
 	const toggleMenu = () => {
-		setIsOpen(!isOpen);
+		setOpened(prev => !prev);
+		if(!animated) {
+			setAnimated(true)
+		}
 	};
 
 	return (
 		<div className={cx(styles.wrapper, className)}>
 			<img src={logoIcon} alt="logo" className={styles.logo}/>
-			<button type="button" className={cx(styles.burgerMenu, {[styles.open]: isOpen})} onClick={toggleMenu}>
+			<button
+				type="button"
+				className={cx(styles.burgerMenu, {
+					[styles.opened]: opened,
+					[styles.animated]: animated,
+				})}
+				onClick={toggleMenu}
+			>
 				<span />
 				<span />
 				<span />
@@ -24,4 +35,4 @@ const Header = ({ className = '' }) => {
 	);
 };
 
-export default Header;
+export default memo(Header);
